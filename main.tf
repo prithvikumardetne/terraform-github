5e871c66f45463d21cef46e73ff80c8441eef57c
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2025-01-04 14:31:46 -0600

    committing files

--- main.tf
+++ main.tf
@@ -11,7 +11,18 @@ provider "github" {
   # Configuration options
 }
 
-resource "github_branch" "development" {
-  repository = "terraform-github"
-  branch     = "dev"
+resource "github_repository" "terraform-github" {
+  name        = "terraform-github"
+  description = "My awesome codebase"
+  auto_init   = true
+}
+
+resource "github_branch" "main" {
+  repository = github_repository.terraform-github.terraform-github_branch
+  branch     = "main"
+}
+
+resource "github_branch_default" "default"{
+  repository = github_repository.terraform-github.terraform-github_branch
+  branch     = github_branch.main.branch
 }
